9665e165f22d379b350ff8bf2403005322a064b2
Daniel Chicotti <danielchicotti2013@gmail.com>
2025-06-13 14:57:52 -0300

    First Commit

---
+++ cypress/e2e/orane-hrm-teste.cy.js
@@ -0,0 +1,31 @@
+describe('Orangge HRM Tests', () => {
+
+  const selectorList  = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']"
+    
+    
+  }
+
+  it('User Info Update - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+
+  })
+
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
+
+  })
+})
