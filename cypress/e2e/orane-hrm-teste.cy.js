58c018e867c1d25ac703eb292676da6763a0d8f1
Daniel Chicotti <danielchicotti2013@gmail.com>
2025-06-13 18:08:07 -0300

    User update scenario added

--- cypress/e2e/orane-hrm-teste.cy.js
+++ cypress/e2e/orane-hrm-teste.cy.js
@@ -8,16 +8,39 @@ describe('Orangge HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboarGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    submitButton: "[type='submit']"
   }
 
-  it('User Info Update - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboarGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('NomeTeste')
+    cy.get(selectorList.middleNameField).clear()
+    cy.get(selectorList.lastNameField).clear().type('Silva')
+    cy.get(':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('3386') // employee ID
+    cy.get(':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('1451') // Other Id
+    cy.get(':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('112233') // Driver License Number
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input').clear().type('2022-20-11') // License Expiry Date
+    cy.get('.--close').click() // Date Close Button
+    cy.get(':nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click() // Nacionality
+    cy.get(':nth-child(27) > span').click() // Brazilian
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click() // Marital Status
+    cy.get(':nth-child(3) > span').click() // Married
+    cy.get(selectorList.submitButton).eq(0).click() // Save button
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close').click()
+
+
 
   })
 
